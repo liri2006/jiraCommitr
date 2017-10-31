b74e5fdc8ef4b58e75e3fb00ae83d0bcd9174395
liri2006 <liri2006@yandex.ru>
2017-10-31 17:07:27 +0100

    add support for Kanban boards

--- content.js
+++ content.js
@@ -9,19 +9,42 @@ function copy(text) {
 
 function getTitle(taskRaw) {
 	var task = $(taskRaw);
+
+	var taskPart = "";
+	if (task.length > 0) {
+		var taskNum = task.attr("data-issue-key");
+		var taskTitle = task.find("section.ghx-summary").text();
+		taskPart = taskNum + ": " + taskTitle;
+	}
+
+	var storyPart = "";
 	var story = task.parents(".ghx-swimlane");
-	var storyTitleEl = story.find(".ghx-heading .ghx-summary");
-	var storyNum = story.find(".ghx-heading .ghx-parent-key").text();
-	var storyTitle = storyTitleEl.contents().not(storyTitleEl.children()).text();
-	var taskNum = task.attr("data-issue-key");
-	var taskTitle = task.find("section.ghx-summary").text();
-	return storyNum + ": " + storyTitle + " - " + taskNum + ": " + taskTitle;
+	if (story.length > 0) {
+		var storyTitleEl = story.find(".ghx-heading .ghx-summary");
+		var storyNum = story.find(".ghx-heading .ghx-parent-key").text();
+		var storyTitle = storyTitleEl.contents().not(storyTitleEl.children()).text();
+		if (storyNum && storyTitle) {
+			storyPart += storyNum + ": " + storyTitle;
+		}
+	}
+
+	if (taskPart && storyPart) {
+		return storyPart + " - " + taskPart;
+	}
+
+	if (taskPart) {
+		return taskPart;
+	}
+
+	return null;
 };
 
 $(function () {
 	$(".ghx-issue").on("click", function () {
 		var title = getTitle(this);
-		console.log(title);
-		copy(title);
+		if (title) {
+			console.log(title);
+			copy(title);
+		}
 	});
 });
