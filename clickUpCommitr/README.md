a50f24129da65c144909f7f9aff43b70529cdeb2
Andrew Munkácsi <a.munkachi@revelane.com>
2021-05-13 20:30:02 +0200

    Add ClickUp implementation

---
+++ clickUpCommitr/README.md
@@ -0,0 +1,4 @@
+# jiraCommitr
+Chrome extension to copy story and task title on task click to be used as commit message
+
+Available on the Chrome Web Store: https://chrome.google.com/webstore/detail/jira-commitr/lkfkeicofolggbehebmjcglhdniglpmd
---
+++ clickUpCommitr/content.js
@@ -0,0 +1,62 @@
+function copy(text) {
+	var input = document.createElement('input');
+	input.setAttribute('value', text);
+	document.body.appendChild(input);
+	input.select();
+	document.execCommand('copy');
+	document.body.removeChild(input)
+}
+
+function getTitle(taskRaw) {
+	var task = $(taskRaw);
+
+	var taskPart = getTaskPart(task);
+	var storyPart = getStoryPart(task);
+
+	return taskPart && storyPart && taskPart !== storyPart ?
+		storyPart + " - " + taskPart :
+		taskPart;
+};
+
+function getTaskPart(task) {
+	if (task.length === 0) {
+		return null;
+	}
+	var taskNum = task.attr("data-task");
+	var taskTitle = task.find(".cu-task-row-main__link-text-inner").text();
+	return "#" + taskNum + ": " + taskTitle;
+}
+
+function getStoryPart(task) {
+	var story = task.parents(".cu-task-list__outer");
+	if (story.length === 0) {
+		return null;
+	}
+
+	story = story.find("> .cu-task-list__rows > .cu-task-row > .task-row__container");
+	if (story.length === 0) {
+		return null;
+	}
+
+	var storyNum = story.attr("data-task");
+	var storyTitle = story.find("> .cu-task-row__container .cu-task-row-main__link-text-inner").text();
+
+	return storyNum && storyTitle ?
+		"#" + storyNum + ": " + storyTitle :
+		"";
+}
+
+$(function () {
+	$(document).on("click", ".cu-task-row__container", function (e) {
+		if($(e.target).hasClass('cu-task-row__arrow') )
+		{
+			return;
+		}
+
+		var title = getTitle(this);
+		if (title) {
+			console.log(title);
+			copy(title);
+		}
+	});
+});
